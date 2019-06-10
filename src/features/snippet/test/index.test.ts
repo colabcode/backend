96268cbaa572cd5576202112b096175886be6e00
Vadym Zhufiak <vzhufk@gmail.com>
2019-06-10 12:16:59 +0300

    [:white_check_mark:] Add resolver tests.

--- src/features/snippet/test/index.test.ts
+++ src/features/snippet/test/index.test.ts
@@ -1,4 +1,5 @@
 import Snippet from "../model";
+import resovler from "../resolve";
 
 describe("snippet", () => {
   describe("objection model", () => {
@@ -8,6 +9,28 @@ describe("snippet", () => {
 
     test("table id", () => {
       return expect(Snippet.idColumn).toBe("id");
+    });
+  });
+
+  describe("resovler", () => {
+    beforeEach(() => {
+      const findById = jest.fn().mockImplementation(v => v);
+      const query = jest.fn().mockReturnValue({
+        findById
+      });
+      Snippet.query = query;
+    });
+
+    test("one", async () => {
+      const id = "uuid";
+      const result = await resovler.get.one({}, { id }, {});
+      expect(Snippet.query).toHaveBeenCalledTimes(1);
+      expect(result).toBe(id);
+    });
+
+    test("all", async () => {
+      await resovler.get.all({}, {}, {});
+      expect(Snippet.query).toHaveBeenCalledTimes(1);
     });
   });
 });
